8e2f988352ac129c79b9f444c9c4d7e17bfcc185
Victor <victorsl96@gmail.com>
2022-05-17 00:05:44 -0300

    Cards criados

--- app.py
+++ app.py
@@ -57,46 +57,65 @@ fig2.update_layout(title='Volume',
 
 app.layout = dbc.Container(
     dbc.Row([
-        dbc.Col([
-            html.Div([
-                html.Img(id='logo',src=app.get_asset_url('logo.png'), height=50),
-                html.H5('Bitcoin price history'),
-                dbc.Button('BTC',color='warning', id='btc_button',size='lg')
-                     ], style={'margin-top':'40px','margin-bottom':'40px','margin-left':'40px','margin-right':'40px'}),
-            
-            html.Div([
-                dcc.DatePickerSingle(id = 'date-picker',
-                                     min_date_allowed=df['Date'].min(),
-                                     max_date_allowed=df['Date'].max(),
-                                     display_format='MMMM D, YYYY',
-                                     date = df['Date'].max(),
-                                     style={'border':'0px solid black'}
-                                )
-            ])
-        ]),
+        dbc.Row([
+            dbc.Col([
+                html.Div([
+                    html.Img(id='logo',src=app.get_asset_url('https://conteudo.imguol.com.br/c/noticias/c2/2022/01/14/bitcoin-grafico-economia-1642179101668_v2_4x3.jpg'), height=50),
+                    html.H1('Bitcoin price history'),
+                    dbc.Button('BTC',color='warning', id='btc_button',size='lg')
+                        ], style={'margin-top':'40px','margin-bottom':'40px','margin-left':'40px','margin-right':'40px'}),
+                   ]),
     
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    html.span #50:41 
-                ])
-            ])
+        dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        html.Span('Preço máximo'),
+                        html.H3(style={'color':'#adfc92'},id='preco-max-text'),
+                        html.Span('Em:'), 
+                        html.H5(id='em-preco-text'),
+                    ])
+                ], color='light',outline=True,style={'margin-top':'10px','box-shadow':'0 4px 0 rgba(0,0,0,0,15), 0 4px 20px 0 rgba(0,0,0,0,0.19)',
+                                                    'color':'#FFFFFF'})
+            ], md=4),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        html.Span('Volume máximo'),
+                        html.H3(style={'color':'#389fd6'},id='volume-max-text'),
+                        html.Span('Em:'), 
+                        html.H5(id='em-volume-text'),
+                    ])
+                ], color='light',outline=True,style={'margin-top':'10px','box-shadow':'0 4px 0 rgba(0,0,0,0,15), 0 4px 20px 0 rgba(0,0,0,0,0.19)',
+                                                    'color':'#FFFFFF'})
+            ], md=4),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        html.Span('Tendência'),
+                        html.H3(style={'color':'#adfc92'},id='tendencia-text'),
+                        html.Span('Desde:'), 
+                        html.H5(id='em-tendencia-text'),
+                    ])
+                ], color='light',outline=True,style={'margin-top':'10px','box-shadow':'0 4px 0 rgba(0,0,0,0,15), 0 4px 20px 0 rgba(0,0,0,0,0.19)',
+                                                    'color':'#FFFFFF'})
+            ], md=4),
         ]),
-    ]),
 
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='line_chart',figure=fig,style={'margin-top':'40px','margin-bottom':'40px','margin-left':'40px','margin-right':'40px'})
-        ])]),
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(id='line_chart',figure=fig,style={'margin-top':'40px','margin-bottom':'40px'})
+                    ])
+                ]),
 
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='volume_chart',figure=fig2,style={'margin-top':'40px','margin-bottom':'40px','margin-left':'40px','margin-right':'40px'})
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(id='volume_chart',figure=fig2,style={'margin-top':'40px','margin-bottom':'40px'})
+                    ])
+                ])
         ])
-    ])
-    ])
-)
+    ], className="g-0")
+, fluid=True)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
